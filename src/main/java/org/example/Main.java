e619297f95583206274c8aedce0346b15733cb28
Sasha <sashashtokalov@bk.ru>
2024-03-21 22:16:43 +0300

    хуй

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+while (true){
+    printMenu();
+}
+    }
+    static void printMenu(){
+fwg
+    }
+}
